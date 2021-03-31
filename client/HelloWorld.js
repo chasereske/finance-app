f6558be504fe73efafeb58b2af9aeeb9c71683bb
chasereske <chasereske@gmail.com>
2021-03-30 21:21:00 -0400

    Added the initial rendering for React and an example page to troubleshoot with to ensure eveverything is working

---
+++ client/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { hot } from 'react-hot-loader'
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>HelloWorld</h1>
+        </div>
+    )
+}
