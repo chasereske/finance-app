a1d3da5a733be73da254b5028ee1493deef8da9c
chasereske <chasereske@gmail.com>
2021-04-08 20:47:24 -0400

    Imported the configured express app to start the server.

--- server/server.js
+++ server/server.js
@@ -3,28 +3,13 @@ import devBundle from './devBundle';
 import path from 'path';
 import template from './../template';
 import { MongoClient } from 'mongodb';
+import config from './../config/config';
+import app from './express';
 
-
-const app = express();
-devBundle.compile(app);
-
-const CURRENT_WORKING_DIR = process.cwd();
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
-
-app.get('/', (req, res) => {
-    res.status(200).send(template())
-})
-
-let port = process.env.PORT || 3000;
-app.listen(port, function onStart (err) {
+app.listen(config.port, (err) => {
     if (err) {
-        console.log(err)
+        console.log(err);
     }
-    console.info('Server started on port %s.', port)
+    console.info('Server started on port %s.', config.port);
 })
 
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/finance-app';
-MongoClient.connect(url, { useNewURLParser: true, useUnifiedTopology: true },(err, db) => {
-    console.log("Connected successfully to mongodb server");
-    //db.close();
-})
