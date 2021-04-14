0a3beac8fad95271c81e058f581114a21764e037
chasereske <chasereske@gmail.com>
2021-04-13 20:24:21 -0400

    Fixed the error with connecting to the backend. Fully functional as of now

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || "YOU_secret_key",
-    mongoURI: process.env.MONGODB_URI ||
-        process.env.MONGO_HOST ||
-        "mongodb+srv://chase123:IamInnovation33!!@cluster0.jxwul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-}
-
-export default config; 
+    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+    mongoUri: process.env.MONGODB_URI ||
+      process.env.MONGO_HOST ||
+      'mongodb+srv://chase123:IamInnovation33!!@cluster0.jxwul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  }
+  
+  export default config
